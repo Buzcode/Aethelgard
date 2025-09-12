16e53541161a8a609835e43de171f04c6de0a5b3
Buzcode <152021325+Buzcode@users.noreply.github.com>
2025-09-12 21:06:47 +0600

    sidebar update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 // Import Layout and Page components
+<<<<<<< Updated upstream
 import MainLayout from './components/MainLayout';
 import HomePage from './pages/HomePage';
 import PeoplePage from './pages/PeoplePage';
@@ -11,11 +12,35 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+=======
+import MainLayout from "./components/MainLayout";
+import HomePage from "./pages/HomePage";
+import PeoplePage from "./pages/PeoplePage";
+import PlacesPage from "./pages/PlacesPage";
+import EventsPage from "./pages/EventsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import CategoryPage from "./pages/CategoryPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import AddArticlePage from "./pages/AddArticlePage";
+import AdminRoute from "./components/AdminRoute";
+>>>>>>> Stashed changes
 
 function App() { 
   return ( 
     <Routes>
+<<<<<<< Updated upstream
       {/* All pages using the MainLayout will be nested inside this Route */}
+=======
+      {/* --- ROUTES WITHOUT SIDEBAR --- */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      {/* --- ROUTES WITH SIDEBAR (using MainLayout) --- */}
+>>>>>>> Stashed changes
       <Route path="/" element={<MainLayout />}>
         
         {/* HomePage will be shown at the root "/" path */}
@@ -28,12 +53,18 @@ function App() {
         {/* These routes are correct based on your homepage links */}
         <Route path="places" element={<PlacesPage />} />
         <Route path="events" element={<EventsPage />} />
+<<<<<<< Updated upstream
 
         {/* Other pages */}
         <Route path="login" element={<LoginPage />} /> 
         <Route path="register" element={<RegisterPage />} />
         <Route path="about" element={<AboutPage />} />    
         <Route path="contact" element={<ContactPage />} /> 
+=======
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Route>
+>>>>>>> Stashed changes
 
         {/* The <Chat/> route was removed because the ChatWidget is already in MainLayout */}
       </Route> 
